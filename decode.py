3b445b9200202eaf58c51c2c7b742e6f45223995
dylanLe <dylan@pi.com>
2018-09-13 06:22:35 +0000

    now is able to produce a UID number and outputs to a file.

--- decode.py
+++ decode.py
@@ -1,5 +1,14 @@
 import subprocess
-var = subprocess.check_output('./quick_start_example1').decode('utf-8').strip();
-output = open('output', 'w+');
-output.write(var);
+output = open('output', 'w+');#Check for output file, if none make one and give write privielges.
+var = subprocess.check_output('./quick_start_example1').decode('utf-8').strip(); #declare variable var, read the output of quick_start_example1 script and decode it from the UTF-8 format and strip it of any extra trailing characters like line breaks
+hex_list = var.split();#takes the string, splits it by spaces, and makes it into a list
+for x in range(len(hex_list)): #for every hex number in the list go through it and turn it into a decimal interger
+    hex_list[x] = int(hex_list[x], 16)
+nfcUID = hex_list[3]; #Creating a unique code to identify a card, take the decimals and bitshift them around
+nfcUID <<= 8; nfcUID |= hex_list[2];#Shift left 8 bits, += the third decimal
+nfcUID <<= 8; nfcUID |= hex_list[1];#etc
+nfcUID <<= 8; nfcUID |= hex_list[0];#etc
+#print(nfcUID)
+#print(hex_list)
+output.write(str(nfcUID));
 
