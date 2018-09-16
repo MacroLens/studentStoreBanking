d2ff6ccdf4c36f19e0d81a4ffa12ca3c13b6ffbb
dylanLe <dylan@pi.com>
2018-09-16 02:27:00 +0000

    new comments

--- sql.py
+++ sql.py
@@ -1,12 +1,19 @@
 import mysql.connector
+#Connect to the mysql server
 mydb = mysql.connector.connect(
         host='192.168.1.135',
         user='test',
         password='test123',
         database='nfc'
         )
-mycursor = mydb.cursor()
-mycursor.execute("SELECT * from ids")
-for x in mycursor:
-    print(x)
 
+#lets me execute things
+mycursor = mydb.cursor()
+#Run any mysql command like this
+mycursor.execute("SELECT * from ids WHERE id_owner = 'Ded man'")
+myresult=mycursor.fetchall() #always returns a list of what was found
+if not myresult:    #if the list is emtpy
+    print('No Results. Create an account?');
+else:
+    for x in myresult:
+        print(x)
