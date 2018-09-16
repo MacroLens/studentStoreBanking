9c8a7a190a32cedda7907f9ce7ba20c5160a694f
Dylan Le <dylan@XPSDylan@mail.org>
2018-09-15 19:55:39 -0500

    testing

--- sql.py
+++ sql.py
@@ -1,11 +1,12 @@
 import mysql.connector
 mydb = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='d092101D'
+        host='192.168.1.131',
+        user='test',
+        password='test123',
+        database='nfc'
         )
 mycursor = mydb.cursor()
-mycursor.execute("SHOW DATABASES")
+mycursor.execute("SELECT * from ids")
 for x in mycursor:
     print(x)
 
