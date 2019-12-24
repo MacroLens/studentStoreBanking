d8cee49221dee9a80befd9a6da5d830b9c6adbc1
Dylan Le <dylan.le@indiansprings.org>
2019-12-23 21:12:03 -0600

    Syntax: Clean up some functions
    Removed unnecessary stuff.

--- gui.py
+++ gui.py
@@ -7,9 +7,7 @@ frame.grid(row=0, column=0)
 textBox = Text(frame)
 textBox.grid(row=0,column=4,rowspan=4)
 textBox.insert(INSERT, "Welcome to the store!")
-def clear():
-    textBox.delete(1.0,END)
-clearText = Button(frame, text="Clear", command=clear).grid(row=4,column=4)
+clearText = Button(frame, text="Clear", command=lambda: textBox.delete(1.0,END)).grid(row=4,column=4)
 
 def printMessage(text):
     textBox.insert(END, "\n"+str(text))
@@ -19,19 +17,21 @@ def printMessage(text):
 
 keypad = [7,8,9,4,5,6,1,2,3,0]
 
-
-r = 1
-c = 0
-for i in keypad:
-    if r==4:
-        c = 1
-    valuePress = lambda button=i: printMessage(button)
-    Button(frame, text=i, command=valuePress, height=10, width=15).grid(row=r,column=c)
-    c+=1
-    if c >= 3:
+class keyPad:
+    def __init__(self,myFrame):
+        r = 1
         c = 0
-        r+=1
+        for i in keypad:
+            if r==4:
+                c = 1
+            valuePress = lambda button=i: printMessage(button)
+            Button(myFrame, text=i, command=valuePress, height=5, width=15).grid(row=r,column=c)
+            c+=1
+            if c >= 3:
+                c = 0
+                r+=1
     
+keypad = keyPad(frame)
 
 
 root.mainloop()
