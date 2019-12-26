cda6b2717a9a9819585ee5c7236b81579d4a994e
Dylan Le <dylan.le@indiansprings.org>
2019-12-26 17:24:52 -0600

    feat: Change returns
    Returns no longer pass in data passed to
    the functions for database.py

--- database.py
+++ database.py
@@ -18,16 +18,16 @@ class database:
         return result  # returns a tuple with data from procedure call.
 
     def getBalance(self, id):
-        return self.__callProcedure("GetBalance", (id, 0, 0))  # The procedure returns on zeros. Balance, Processed?
+        return self.__callProcedure("GetBalance", (id, 0, 0))[-2:]  # The procedure returns on zeros. Balance, Processed?
 
     def getStudentName(self, id):
-        return self.__callProcedure("GetStudentName", (id, 0, 0))  # Name, Processed?
+        return self.__callProcedure("GetStudentName", (id, 0, 0))[-2:]  # Name, Processed?
 
     def changeBalance(self, id, difference):
-        return self.__callProcedure("ChangeBalance", (id, difference, 0))  # Processed?
+        return self.__callProcedure("ChangeBalance", (id, difference, 0))[-1:]  # Processed?
 
     def addAccount(self, id, name, balance):
-        return self.__callProcedure("AddAccount", (id, name, balance, 0))  # Processed?
+        return self.__callProcedure("AddAccount", (id, name, balance, 0))[-1:]  # Processed?
 
     def closeConnection(self):
         self.cursor.close()  # close the connections
